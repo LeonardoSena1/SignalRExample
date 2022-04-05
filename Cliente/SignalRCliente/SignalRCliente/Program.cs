6b7d8f31cb90d8ce817dacb4f2f715456a2d7598
Leonardo Sena <leonardo_sena1@hotmail.com>
2022-04-05 12:55:25 -0300

    FirstCommit

---
+++ Cliente/SignalRCliente/SignalRCliente/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalRCliente
+{
+    class Program
+    {
+        const string Url = "http://localhost:49778/chat";
+        static async Task Main(string[] args)
+        {
+            await using var connection = new HubConnectionBuilder().WithUrl(Url).Build();
+
+            await connection.StartAsync();
+
+            await foreach(var date in connection.StreamAsync<DateTime>("Streaming"))
+            {
+                Console.WriteLine(date);
+            }
+        }
+    }
+}
---
+++ Server/MyHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class MyHub : Hub
+    {
+        public async IAsyncEnumerable<DateTime> Streaming(CancellationToken token)
+        {
+            while (true)
+            {
+                yield return DateTime.Now;
+                await Task.Delay(1000, token);
+            }
+        }
+    }
+}
